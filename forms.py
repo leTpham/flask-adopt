4b88e2ad4bc41b76ecfa14d00acbb8255a32924b
Le Pham <phamtle724@gmail.com>
2022-08-05 15:12:00 -0700

    working on AddPetForm in forms.py

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 """Forms for adopt app."""
 from flask_wtf import FlaskForm
-from wtforms import StringField, RadioField, SelectField, TextAreaField, InputRequired, Optional, Email
+from wtforms import StringField, RadioField, SelectField, TextAreaField, InputRequired, Optional, URL
 
 """
 class for Add pet form
@@ -18,4 +18,16 @@ class AddPetForm(FlaskForm):
 
     name = StringField("Pet Name",
                         validators=[InputRequired()] )
-    species = StringField("Species")
+    species = SelectField("Species",
+                            choices = [('cat', 'Cat'),
+                             ('dog', 'Dog'),
+                              ('porcupine', 'Porcupine')])
+    photo_url = StringField("Photo URL",
+                               validators=[Optional(), URL()])
+    age = SelectField("Age",
+                            choices = [('baby', 'Baby'),
+                                    ('young', 'Young'),
+                                    ('adult', 'Adult'),
+                                    ('senior', 'Senior')]
+    notes = TextAreaField('Notes')
+
